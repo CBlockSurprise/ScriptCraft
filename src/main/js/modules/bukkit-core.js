5bc6a5bbdb480bc8bb6de6ccef9a59bd7ea9c544
aaron-powell <aaron@mvcodeclub.com>
2015-10-19 11:44:40 -0700

    added bukkit-core module

---
+++ src/main/js/modules/bukkit-core.js
@@ -0,0 +1,5 @@
+if (__plugin.canary) {
+  console.log("bukkit module cannot be used with CanaryMod server");
+} else {
+  module.exports = require('./bukkit/bukkit');
+}
---
+++ src/main/js/modules/bukkit/bukkit-core.js
@@ -0,0 +1,12 @@
+var bkBukkit = org.bukkit.Bukkit;
+
+// broadcast message
+exports.broadcastMessage = function (msg) {
+	bkBukkit.broadcastMessage(msg);
+}
+
+
+// execute console command
+exports.consoleCommand = function (cmd) {
+	bkBukkit.dispatchCommand(server.consoleSender, cmd);
+}
