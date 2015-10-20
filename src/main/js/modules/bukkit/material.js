a6af8f17316862b70f27a64e486b56eb2c4dae82
aaron-powell <aaron@mvcodeclub.com>
2015-10-20 13:40:43 -0700

    finished material.js module

--- src/main/js/modules/bukkit/material.js
+++ src/main/js/modules/bukkit/material.js
@@ -1,3 +1,9 @@
 var bkMaterial = Packages.org.bukkit.Material;
 
 var materials = bkMaterial.values();
+
+for (var i = 0; i < materials.length; i++) {
+	var name = (''+materials[i].name()).toLowerCase();
+	name = name.replace(/(_.)/g,function(a){ return a.replace(/_/,'').toUpperCase(); });
+	exports[name] = materials[i];
+}
