9fad6e40eb446f9bee178621cda7d5ffb21b9046
aaron-powell <aaron@mvcodeclub.com>
2015-10-20 14:54:27 -0700

    added game-mode.js module

---
+++ src/main/js/modules/bukkit/game-mode.js
@@ -0,0 +1,9 @@
+var bkSurvival = org.bukkit.GameMode.SURVIVAL;
+var bkCreative = org.bukkit.GameMode.CREATIVE;
+var bkSpectator = org.bukkit.GameMode.SPECTATOR;
+var bkAdventure = org.bukkit.GameMode.ADVENTURE;
+
+exports.survival = bkSurvival;
+exports.creative = bkCreative;
+exports.spectator = bkSpectator;
+exports.adventure = bkAdventure;
