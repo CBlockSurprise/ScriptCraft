8a1ac4203b7d99170c6bbc8b692b50831d022c61
aaron-powell <aaron@mvcodeclub.com>
2015-12-16 14:12:25 -0800

    added PluginList.js which provides the /js plugins() function

---
+++ src/main/js/plugins/PluginList.js
@@ -0,0 +1,18 @@
+exports.plugins = function() {
+    var curDir = java.lang.System.getProperty("user.dir");
+    var path = "/home/" + curDir.split("/")[5] + "/server/scriptcraft/plugins";
+    var dir = new java.io.File(path);
+    var filesList = dir.listFiles();
+    var pluginList = [];
+    for (var child in filesList) {
+        if (filesList[child].isDirectory()) {
+            var name = filesList[child].getName();
+            var drone = new java.lang.String("drone");
+            var list = new java.lang.String("PluginList");
+            if (!(name.equals(drone) || name.equals(list))) {
+                pluginList.push(" " + name);
+            }
+        }
+    }
+    echo("Plugins: ".aqua() + pluginList.toString().white());
+};
