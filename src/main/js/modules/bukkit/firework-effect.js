23f29577a0c9aaba11442865ce47c430452d5375
Aaron <aaron@mvcodeclub.com>
2016-03-09 12:45:45 -0800

    added v1 of firework-effect module

--- src/main/js/modules/bukkit/firework-effect.js
+++ src/main/js/modules/bukkit/firework-effect.js
@@ -1,3 +1,26 @@
 var bkFireworkEffect = org.bukkit.FireworkEffect;
 
-// TODO
+exports.type = 
+			{"ball": 		bkFireworkEffect.Type.BALL,
+			 "largeBall": 	bkFireworkEffect.Type.BALL_LARGE,
+			 "burst":  		bkFireworkEffect.Type.BURST,
+			 "creeper": 	bkFireworkEffect.Type.CREEPER,
+			 "star": 		bkFireworkEffect.Type.STAR
+			};
+
+exports.setFireworkEffect = function(firework, properties) {
+
+	var meta = firework.fireworkMeta;
+
+	var effect = bkFireworkEffect.builder()
+									.flicker(properties.flicker)
+									.withColor(properties.mainColor)
+									.withFade(properties.fadeColor)
+									.with(properties.fireworkType)
+									.trail(properties.trail)
+									.build();
+
+	meta.addEffect(effect);
+	meta.setPower(properties.power);
+	firework.fireworkMeta = meta;
+}
