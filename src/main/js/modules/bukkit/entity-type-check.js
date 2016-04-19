a90a0cb3a4fe79ab37507be418c216624910d484
Aaron <aaron@mvcodeclub.com>
2016-04-18 18:08:50 -0700

    fixed null reference error in entity-type-check module

--- src/main/js/modules/bukkit/entity-type-check.js
+++ src/main/js/modules/bukkit/entity-type-check.js
@@ -1,264 +1,330 @@
 var bkEntityType = org.bukkit.entity.EntityType;
 
 function isArmorStand(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ARMOR_STAND);
 }
 
 function isArrow(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ARROW);
 }
 
 function isBat(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.BAT);
 }
 function isBlaze(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.BLAZE);
 }
 
 function isBoat(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.BOAT);
 }
 
 function isCaveSpider(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.CAVE_SPIDER);
 }
 
 function isChicken(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.CHICKEN);
 }
 
 function isComplexPart(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.COMPLEX_PART);
 }
 
 function isCow(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.COW);
 }
 
 function isCreeper(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.CREEPER);
 }
 
 function isDroppedItem(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.DROPPED_ITEM);
 }
 
 function isEgg(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.EGG);
 }
 
 function isEnderCrystal(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ENDER_CRYSTAL);
 }
 
 function isEnderDragon(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ENDER_DRAGON);
 }
 
 function isEnderPearl(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ENDER_PEARL);
 }
 
 function isEnderSignal(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ENDER_SIGNAL);
 }
 
 function isEnderman(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ENDERMAN);
 }
 
 function isEndermite(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ENDERMITE);
 }
 
 function isExperienceOrb(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.EXPERIENCE_ORB);
 }
 
 function isFallingBlock(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.FALLING_BLOCK);
 }
 
 function isFireball(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.FIREBALL);
 }
 
 function isFirework(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.FIREWORK);
 }
 
 function isFishingHook(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.FISHING_HOOK);
 }
 
 function isGhast(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.GHAST);
 }
 
 function isGiant(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.GIANT);
 }
 
 function isGuardian(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.GUARDIAN);
 }
 
 function isHorse(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.HORSE);
 }
 
 function isIronGolem(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.IRON_GOLEM);
 }
 
 function isItemFrame(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ITEM_FRAME);
 }
 
 function isLeashHitch(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.LEASH_HITCH);
 }
 
 function isLightning(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.LIGHTNING);
 }
 
 function isMagmaCube(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MAGMA_CUBE);
 }
 
 function isMinecart(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MINECART);
 }
 
 function isMinecartChest(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MINECART_CHEST);
 }
 
 function isMinecartCommand(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MINECART_COMMAND);
 }
 
 function isMinecartFurnace(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MINECART_FURNACE);
 }
 
 function isMinecartHopper(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MINECART_HOPPER);
 }
 
 function isMinecartMobSpawner(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MINECART_MOB_SPAWNER);
 }
 
 function isMinecartTNT(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MINECART_TNT);
 }
 
 function isMushroomCow(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.MUSHROOM_COW);
 }
 
 function isOcelot(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.OCELOT);
 }
 
 function isPaintning(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.PAINTING);
 }
 
 function isPig(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.PIG);
 }
 
 function isPigZombie(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.PIG_ZOMBIE);
 }
 
 function isPlayer(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.PLAYER);
 }
 
 function isPrimedTNT(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.PRIMED_TNT);
 }
 
 function isRabbit(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.RABBIT);
 }
 
 function isSheep(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SHEEP);
 }
 
 function isSilverfish(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SILVERFISH);
 }
 
 function isSkeleton(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SKELETON);
 }
 
 function isSlime(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SLIME);
 }
 
 function isSmallFireball(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SMALL_FIREBALL);
 }
 
 function isSnowball(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SNOWBALL);
 }
 
 function isSnowman(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SNOWMAN);
 }
 
 function isSpider(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SPIDER);
 }
 
 function isSplashPotion(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SPLASH_POTION);
 }
 
 function isSquid(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.SQUID);
 }
 
 function isThrownExpBottle(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.THROWN_EXP_BOTTLE);
 }
 
 function isUnknown(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.UNKNOWN);
 }
 
 function isVillager(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.VILLAGER);
 }
 
 function isWeather(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.WEATHER);
 }
 
 function isWitch(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.WITCH);
 }
 
 function isWither(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.WITHER);
 }
 
 function isWitherSkull(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.WITHER_SKULL);
 }
 
 function isWolf(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.WOLF);
 }
 
 function isZombie(entity) {
+	if (entity == null) return false;
 	return entity.type.equals(bkEntityType.ZOMBIE);
 }
