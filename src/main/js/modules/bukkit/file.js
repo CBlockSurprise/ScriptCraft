72e948b3af0bf7c26c2e8ac780d7154b02f7c121
Aaron <aaron@mvcodeclub.com>
2016-04-26 12:52:56 -0700

    prevent generation of more than 100 files using file.js module

--- src/main/js/modules/bukkit/file.js
+++ src/main/js/modules/bukkit/file.js
@@ -1,30 +1,18 @@
-
 exports.write = function(filename, string) {
-
 	var mc_path = java.lang.System.getenv("MC_PATH");
-
+	var dataDir = new java.io.File(mc_path + "/plugin_data");
+	if (dataDir.list().length > 99) return;
 	var path = java.nio.file.Paths.get(mc_path + "/plugin_data/" + filename);
-
 	var outStream = new java.io.FileOutputStream(path, false);
-
 	var writer = new java.io.PrintWriter(outStream);
-
 	writer.println(string);
-
 	writer.close();
-
 }
 
-
 exports.load = function(filename) {
-
 	var mc_path = java.lang.System.getenv("MC_PATH");
-
 	var path = java.nio.file.Paths.get(mc_path + "/plugin_data/" + filename);
-
 	var reader = java.nio.file.Files.readAllLines(path);
-
 	var data = new java.lang.String(reader);
-
 	return data;
 }
