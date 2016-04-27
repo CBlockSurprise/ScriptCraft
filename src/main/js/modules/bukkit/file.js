58838b5b44e5f1f639d0b2455d00b6b83694ddf5
aaron-powell <aaron@mvcodeclub.com>
2016-04-27 13:04:40 -0700

    changed too many files message to error

--- src/main/js/modules/bukkit/file.js
+++ src/main/js/modules/bukkit/file.js
@@ -2,7 +2,7 @@ exports.write = function(filename, string) {
 	var mc_path = java.lang.System.getenv("MC_PATH");
 	var dataDir = new java.io.File(mc_path + "/plugin_data");
 	if (dataDir.list().length > 99) {
-		console.log("Too many files! Limit is 100 per server.");
+		console.error("Too many files! Limit is 100 per server.");
 		return;
 	}
 	var path = java.nio.file.Paths.get(mc_path + "/plugin_data/" + filename);
