3fb067c322a98730016948d1f0a30b862702b28c
aaron-powell <aaron@mvcodeclub.com>
2016-10-11 13:29:07 -0700

    updated bukkit-core with procedurally exported methods

--- src/main/js/modules/bukkit/bukkit-core.js
+++ src/main/js/modules/bukkit/bukkit-core.js
@@ -1,12 +1,26 @@
 var bkBukkit = org.bukkit.Bukkit;
 
-// broadcast message
-exports.broadcastMessage = function(msg) {
-	bkBukkit.broadcastMessage(msg);
-}
+var methods = bkBukkit.class.getDeclaredMethods();
 
+for (var i in methods) {
+  var methodName = methods[i].getName();
+  var params = methods[i].getParameterTypes();
+  var numParams = params.length;
+  var func = methods[i];
+  exports[methodName] = function(idx) {
+    return function() {
+      var args = [];
+      for (var index in arguments) {
+        args.push(arguments[index])
+      }
 
-// execute console command
-exports.consoleCommand = function(cmd) {
-	bkBukkit.dispatchCommand(server.consoleSender, cmd);
+      var argStrings = args.map(function(val, key) {
+        return 'args['+key+']';
+      })
+
+      return eval('org.bukkit.Bukkit.'+methods[idx].getName()+'('+argStrings.join(', ')+')');
+    }
+    
+  }(i);
+  
 }
