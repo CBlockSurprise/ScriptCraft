c8ce301a48d5875386019eb38f62cbfed771d041
Aaron <aaron@mvcodeclub.com>
2016-10-31 12:09:48 -0700

    moved powered by mvcode message to its own file

---
+++ src/main/js/plugins/aaaaaModuleLoader/MVCodePowered.js
@@ -0,0 +1,8 @@
+// sends players "powered by MVCode" message when they log in
+var onPlayerJoin = function(event) {
+	var player = event.player;
+	setTimeout(function() {echo(player, "Welcome!".green() + 
+		" This server is powered by ".white() + "M".blue()+"V".aqua()+"C".brightgreen()+"o".yellow()+"d".gold()+"e".red());},
+		500);
+}
+events.playerJoin(onPlayerJoin);
--- src/main/js/plugins/aaaaaModuleLoader/aaaaaModuleLoader.js
+++ src/main/js/plugins/aaaaaModuleLoader/aaaaaModuleLoader.js
@@ -1,12 +1,3 @@
 // plugins are loaded in alphabetical order
 // this plugin ensures modules are loaded before attempting to load other plugins
 load("scriptcraft/modules/bukkit/master.js");
-
-// sends players "powered by MVCode" message when they log in
-var onPlayerJoin = function(event) {
-	var player = event.player;
-	setTimeout(function() {echo(player, "Welcome!".green() + 
-		" This server is powered by ".white() + "M".blue()+"V".aqua()+"C".brightgreen()+"o".yellow()+"d".gold()+"e".red());},
-		500);
-}
-events.playerJoin(onPlayerJoin);
